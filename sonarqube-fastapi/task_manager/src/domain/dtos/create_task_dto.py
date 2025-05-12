c7457f5a9ca5a0ea8f667c45da0e9eb814fff230
naolca <firaolibrahim28@gmail.com>
2025-05-12 20:31:46 +0300

    dto implementation update

--- sonarqube-fastapi/task_manager/src/domain/dtos/create_task_dto.py
+++ sonarqube-fastapi/task_manager/src/domain/dtos/create_task_dto.py
@@ -1,5 +1,6 @@
 from typing import TypedDict
 
-
+# Data Transfer Object (DTO) for creating a new task
 class CreateTaskDto(TypedDict):
+    # Description of the task to be created
     description: str
--- sonarqube-fastapi/task_manager/src/domain/dtos/update_task_dto.py
+++ sonarqube-fastapi/task_manager/src/domain/dtos/update_task_dto.py
@@ -1,6 +1,8 @@
 from typing import NotRequired, TypedDict
 
-
+# Data Transfer Object for updating a task
 class UpdateTaskDto(TypedDict):
+    # Optional description of the task
     description: NotRequired[str]
+    # Optional completion status of the task
     completed: NotRequired[bool]
