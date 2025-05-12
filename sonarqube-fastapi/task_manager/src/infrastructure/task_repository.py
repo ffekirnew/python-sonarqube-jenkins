ebb52cccc5a11442d398e7f862cebcb94cfec5bf
naolca <firaolibrahim28@gmail.com>
2025-05-12 20:39:59 +0300

    tast repo update with comments

--- sonarqube-fastapi/task_manager/src/infrastructure/task_repository.py
+++ sonarqube-fastapi/task_manager/src/infrastructure/task_repository.py
@@ -4,15 +4,17 @@ from src.domain.contracts import ABCTaskRepository
 from src.domain.dtos import CreateTaskDto, UpdateTaskDto
 from src.domain.task import Task
 
-
+# TaskRepository implements the ABCTaskRepository interface and uses SingletonMeta to ensure a single instance.
 class TaskRepository(ABCTaskRepository, metaclass=SingletonMeta):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Initialize the current task ID and a dictionary to store tasks.
         self._curr_id: int = 1001
         self.tasks: dict[int, Task] = {
             1000: Task(id=1000, description="Sample task", completed=False),
         }
 
+    # Create a new task and add it to the repository.
     def create_task(self, task_data: CreateTaskDto) -> Task:
         new_task: Task = {
             "id": self._curr_id,
@@ -24,12 +26,15 @@ class TaskRepository(ABCTaskRepository, metaclass=SingletonMeta):
 
         return new_task
 
+    # Retrieve all tasks as a list.
     def get_all(self) -> list[Task]:
         return list(self.tasks.values())
 
+    # Retrieve a single task by its ID.
     def get(self, task_id: int) -> Task | None:
         return self.tasks.get(task_id)
 
+    # Delete a task by its ID. Raises an exception if the task does not exist.
     def delete_task(self, task_id: int) -> Task:
         if task_id not in self.tasks:
             raise ApplicationException(
@@ -43,6 +48,7 @@ class TaskRepository(ABCTaskRepository, metaclass=SingletonMeta):
 
         return task
 
+    # Update an existing task with new data. Raises an exception if the task does not exist.
     def update_task(
         self,
         task_id: int,
