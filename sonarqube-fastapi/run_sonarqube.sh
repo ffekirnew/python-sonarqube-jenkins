b0de5d2674e2f1c44b8944892a3270a8cdcaf17b
ffekirnew <ffekirnew0808@gmail.com>
2025-05-18 02:18:02 +0300

    Update run_sonarqube.sh

--- sonarqube-fastapi/run_sonarqube.sh
+++ sonarqube-fastapi/run_sonarqube.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PROJECT_BASE_DIR=test_manager
 SONARQUBE_URL=http://localhost:9000
 SONARQUBE_PROJECT_KEY=fastapi-sonarqube
@@ -5,8 +7,8 @@ SONARQUBE_TOKEN=sqp_319836e0c8adee09481c497803d4ebcf26b522e8
 
 docker run \
 		--network=host \
-		-v $(PWD):/$(PROJECT_BASE_DIR) \
-		-e SONAR_HOST_URL="$(SONARQUBE_URL)" \
-		-e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$(SONARQUBE_PROJECT_KEY) -Dsonar.login=$(SONARQUBE_TOKEN) -Dsonar.projectBaseDir=/$(PROJECT_BASE_DIR)" \
+		-v $PWD:/$PROJECT_BASE_DIR \
+		-e SONAR_HOST_URL="$SONARQUBE_URL" \
+		-e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$SONARQUBE_PROJECT_KEY -Dsonar.login=$SONARQUBE_TOKEN -Dsonar.projectBaseDir=/$PROJECT_BASE_DIR" \
 		sonarsource/sonar-scanner-cli	
 
